9d4f4e4d23395fb04f3f508df5f5e9d6ededff7a
Rashid Khan <rashood.khan@gmail.com>
2014-01-04 18:57:41 +0530

    Removed index.html in admin, Modified db_connect for allowing connections

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,7 +1,7 @@
 <?php
-		$db_host = "localhost";
-		$db_user = "rashid";
-		$db_password = "Orange20202!";
+		$db_host = "";
+		$db_user = "";
+		$db_password = "";
 		$db_name = "noffct";
 		$connection=@mysql_connect($db_host, $db_user, $db_password)or die("Database Error - Could not create a connection"); 
 		mysql_select_db($db_name,$connection)or die("Database could not be selected");
