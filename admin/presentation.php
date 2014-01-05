f02e87254424c1bcd1726b77977578b27b23293a
Rashid Khan <rashood.khan@gmail.com>
2014-01-05 17:03:43 +0530

    Added Document/Presentation Upload Feature

---
+++ admin/presentation.php
@@ -0,0 +1,112 @@
+<?php
+	session_start();
+	session_regenerate_id();
+	if(!isset($_SESSION['username']))     
+	{
+    	header("Location: login.html");
+	}
+	echo "Succesfully Signed In";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Administrator Panel - Audio Upload</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../css/sticky-footer-navbar.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <!-- Wrap all page content here -->
+    <div id="wrap">
+
+      <!-- Fixed navbar -->
+      <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">Administrative Panel</a>
+          </div>
+          <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="audio.php">Audio/Video</a></li>
+                <li><a href="event.php">Events</a></li>
+			    <li class="active"><a href="presentation.php">Documents</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+
+      <!-- Begin page content -->
+      <div class="container">
+        <div class="page-header">
+          <h1>Document/Presentation Upload Panel</h1>
+        </div>
+        <div class="col-md-4">
+            <form method='POST' enctype="multipart/form-data"  action='presentation_upload.php'>
+        <div id="from-group">
+            <label for="name">Name</label>
+            <input type="text" class="form-control" id="name" name="name" placeholder="Document Name" required>
+        </div>
+          <br>
+          <div id="form-group">
+            <select class="form-control" name="selector">
+              <option value="PPT">Powerpoint Presentation</option>
+                <option value="PDF">PDF Document</option>
+                <option value="DOC">Document</option>
+              </select>
+          </div>
+        <br>
+        <div class="form-group">
+        <label for="fileupload">File input</label>
+        <input type="file" id="fileupload" name="filename" required>
+        <p class="help-block">Browse through your computer and upload file</p>
+        </div>
+        <button type="submit" class="btn btn-default btn-lg btn-block">Click here to upload</button>
+        
+    </form>
+    </div>
+    </div>
+    </div>
+
+    <div id="footer">
+      <div class="container">
+        <p class="text-muted">Developed by MediaMechanic</p>
+      </div>
+    </div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
---
+++ admin/presentation_upload.php
@@ -0,0 +1,125 @@
+<?php
+    session_start();
+	session_regenerate_id();
+	if(!isset($_SESSION['username']))     
+	{
+    	header("Location: login.html");
+	}
+
+    require 'db_connect.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Administrator Panel</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="../css/sticky-footer-navbar.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <!-- Wrap all page content here -->
+    <div id="wrap">
+
+      <!-- Fixed navbar -->
+      <div class="navbar navbar-default navbar-fixed-top" role="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="index.php">Administrative Panel</a>
+          </div>
+          <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="#">Audio/Video</a></li>
+                <li><a href="event.php">Events</a></li>
+			    <li class="active"><a href="presentation.php">Documents</a></li>
+                <li><a href="logout.php">Logout</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div>
+
+      <!-- Begin page content -->
+      <div class="container">
+        <div class="page-header">
+          <h1>Document/Presentation Upload Panel</h1>
+        </div>
+    
+<?php
+
+    $name = $_POST['name'];
+    $file = $_FILES['filename']['name'];
+    $selector = $_POST['selector'];
+
+    
+        $query = "SELECT * FROM `doc_details` WHERE `name` LIKE '".$name."' LIMIT 0 , 30";
+        $search = mysql_query($query);
+        $numRows = mysql_num_rows($search);
+        if($numRows > 0){
+            echo "<p class='lead'>The File $name already exists. Please Upload the file with different name.<br>You are being Re-directed to Document Upload Page</p>";
+            header('Refresh: 5; URL=presentation.php');
+            exit();
+        }
+
+        $updateQuery = "INSERT INTO `noffct`.`doc_details` (`name`,`file_name`,`file_type`,`date_uploaded`) VALUES ('$name','$file','$selector', now())";
+        $resultUpdate = mysql_query($updateQuery);
+        if(!$resultUpdate){
+            echo '<p class="lead">Error Uploading the file. Please try again later.<br>You are being re-directed to Document Upload Page</p>';
+            header('Refresh: 5; URL=presentation.php');
+            exit();
+        }
+    
+        $uploadDir = "docs_uploaded/";
+        if (move_uploaded_file($_FILES['filename']['tmp_name'], "$uploadDir$file") ==  false) {
+            echo '<p class="lead">Error Uploading the file. Please try again later.<br>You are being re-directed to Document Upload Page</p>';
+	       header('Refresh: 5; URL=presentation.php');
+	       exit();
+        }
+
+        echo "<p class='lead'>The file $name has been uploaded successfully.<br>You will be re-directed to Administration Portal now.";
+        header('Refresh: 5; URL=index.php');
+    
+?>
+
+      </div>
+    </div>
+
+    <div id="footer">
+      <div class="container">
+        <p class="text-muted">Developed by MediaMechanic</p>
+      </div>
+    </div>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
